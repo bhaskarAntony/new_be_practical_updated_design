c115edbdc883342513634a1b75c13b4665946c8a
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-09-26 20:28:36 +0530

    my project files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
   }, []);
 
   //model open logic 
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   const handleCloseModal = () => setShowModal(false);
 
