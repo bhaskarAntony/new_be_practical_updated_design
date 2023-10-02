e329853a5eb2df1573ea7839a2ea3559212c62c0
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-10-02 14:41:23 +0530

    my project files

--- src/components/TrendingCourses.jsx
+++ src/components/TrendingCourses.jsx
@@ -3,54 +3,52 @@ import '../styles/allcourses.css'
 // import AOS from 'aos';
 
 var courses = [
-    {
-        id:1,
-        name: "MERN Fullstack",
-        tag:"Trending",
-        duration: "4.5 Months",
-        trainer: "...",
-        languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
-        ratedStudents: "465",
-        rating:"4.5",
-        icon: "https://cdn-icons-png.flaticon.com/128/7991/7991055.png",
-        image: "https://images.pexels.com/photos/3183165/pexels-photo-3183165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    },
-    {
-        id:2,
-        name: "Java Fullstack",
-        tag:"Trending",
-        duration: "4.5 Months",
-        trainer: "...",
-        languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
-        ratedStudents: "465",
-        rating:"4.5",
-        icon: "https://cdn-icons-png.flaticon.com/128/1183/1183669.png",
-        image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    },
-    {
-        id:3,
-        name: "Python Fullstack",
-        duration: "4.5 Months",
-        tag:"Trending",
-        trainer: "...",
-        ratedStudents: "465",
-        rating:"4.5",
-        languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
-        icon: "https://cdn-icons-png.flaticon.com/128/5968/5968350.png",
-        image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    },
-    {
-        id:4,
-        name: "Cloud Oops",
-        duration: "4.5 Months",
-        tag:"Trending",
-        trainer: "...",
-        languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
-        ratedStudents: "465",
-        rating:"4.5",
-        icon: "https://cdn-icons-png.flaticon.com/128/3305/3305673.png",
-        image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-    }
+  {
+      id:1,
+      name: "Complete Fullstack",
+      tag:"Trending",
+      duration: "4.5 Months",
+      trainer: "...",
+      languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
+      ratedStudents: "465",
+      rating:"4.5",
+      icon: "https://cdn-icons-png.flaticon.com/128/7991/7991055.png",
+      image: "https://images.pexels.com/photos/3183165/pexels-photo-3183165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+      id:2,
+      name: "MERN  Fullstack",
+      tag:"Trending",
+      duration: "4.5 Months",
+      trainer: "...",
+      languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
+      ratedStudents: "465",
+      rating:"4.5",
+      icon: "https://cdn-icons-png.flaticon.com/128/1183/1183669.png",
+      image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+      id:4,
+      name: "Cloud Oops",
+      duration: "4.5 Months",
+      trainer: "...",
+      languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
+      ratedStudents: "465",
+      rating:"4.5",
+      icon: "https://cdn-icons-png.flaticon.com/128/3305/3305673.png",
+      image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+  {
+      id:5,
+      name: "Data Science",
+      duration: "4.5 Months",
+      trainer: "...",
+      languages: ["html", "css", "Bootstrap", "Javascript", "React Js", "NodeJs", "Express JS", "MongoDB"],
+      ratedStudents: "465",
+      rating:"4.5",
+      icon: "https://cdn-icons-png.flaticon.com/128/2756/2756778.png",
+      image: "https://images.pexels.com/photos/2102415/pexels-photo-2102415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }
 ]
 
 function TrendingCourses() {
@@ -65,15 +63,17 @@ function TrendingCourses() {
                 <div className="card course-card border" data-aos="zoom-in">
                   <div className="row">
                       <div className="col-12 cl-md-5 col-lg-4">
-                      <div className={`course-card-header p-2 d-flex align-items-end bg-black justify-content-between course-card-header${item.id}`}>
-                <img src="https://media.istockphoto.com/id/1193323373/vector/a-colorful-trendy-card-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=iIqY56kV1nKpfKCceag5NFWBnogRv8ZmJCFiicZxlpU=" alt="" />
+                      <div className={`course-card-header p-2 d-flex align-items-end justify-content-between course-card-header${item.id}`}>
+                      <div className="icon">
+                  <img src={item.icon} alt="" className='w-100 img-fluid' />
+                </div>
                   <div className="curve">
 
                   </div>
                   <div className="blur">
                  
                   </div>
-                     <div className="highlighter p-2">
+                     {/* <div className="highlighter p-2">
                       <p className="p-1 px-3 text-white bg-danger rounded-5"><i class="bi bi-fire"></i>{item.tag}</p>
                      <div className="course-icons">
                        <div className="share">
@@ -83,8 +83,9 @@ function TrendingCourses() {
                       <i class="bi bi-heart"></i>
                       </div>
                      </div>
-                    </div>
+                    </div> */}
                 </div>
+
                       </div>
                       <div className="col-12 col-md-7 col-lg-8">
                <div className="course-card-body p-2">
